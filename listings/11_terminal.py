286aff464e4795f76a550458699c6222ce9cf54c
encarsia <An.Ke@bahnfreikartoffelbrei.de>
2016-11-30 18:50:59 +0100

    Nikola auto commit.
    Source commit: b83b867f5ba548c4b0255dad307ddd1356fdf66a
    Nikola version: 7.8.1

---
+++ listings/11_terminal.py
@@ -0,0 +1,45 @@
+import os
+import gi
+gi.require_version('Gtk','3.0')
+gi.require_version('Vte', '2.91')
+
+from gi.repository import Gtk,Vte,GObject,GLib
+
+class Handler:
+
+    def on_window_destroy(self,*args):
+        Gtk.main_quit()
+
+    def on_button_clicked(self,widget):
+        command = "python\n"
+        x.terminal.feed_child(command,len(command))
+
+class Example:
+    
+    def __init__(self):
+        
+        self.builder = Gtk.Builder()
+        GObject.type_register(Vte.Terminal)
+        
+        self.builder.add_from_file("11_terminal.glade")
+        self.builder.connect_signals(Handler())
+        
+        self.terminal = self.builder.get_object("term")
+        self.terminal.spawn_sync(
+            Vte.PtyFlags.DEFAULT,
+            None,
+            ["/bin/bash"],
+            None,
+            GLib.SpawnFlags.DEFAULT,
+            None,
+            None,
+            )
+
+        window = self.builder.get_object("window")
+        window.show_all()
+
+    def main(self):
+        Gtk.main()
+
+x = Example()
+x.main()
