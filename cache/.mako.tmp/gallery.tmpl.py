3157843d69263853c4230b2ff925aa15ed6e9e04
encarsia <An.Ke@bahnfreikartoffelbrei.de>
2019-03-15 22:17:50 +0100

    Nikola auto commit.
    Nikola version: 8.0.2

--- cache/.mako.tmp/gallery.tmpl.py
+++ cache/.mako.tmp/gallery.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1552674588.9751928
+_modified_time = 1552684667.61608
 _enable_loop = True
 _template_filename = '/usr/lib/python3.7/site-packages/nikola/data/themes/base/templates/gallery.tmpl'
 _template_uri = 'gallery.tmpl'
@@ -33,32 +33,32 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def extra_js():
-            return render_extra_js(context._locals(__M_locals))
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
-        _link = context.get('_link', UNDEFINED)
-        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
+        lang = context.get('lang', UNDEFINED)
         len = context.get('len', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
+        gallery_path = context.get('gallery_path', UNDEFINED)
         parent = context.get('parent', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
         def sourcelink():
             return render_sourcelink(context._locals(__M_locals))
-        comments = _mako_get_namespace(context, 'comments')
-        folders = context.get('folders', UNDEFINED)
-        permalink = context.get('permalink', UNDEFINED)
-        def extra_head():
-            return render_extra_head(context._locals(__M_locals))
-        gallery_path = context.get('gallery_path', UNDEFINED)
-        enable_comments = context.get('enable_comments', UNDEFINED)
-        photo_array = context.get('photo_array', UNDEFINED)
-        photo_array_json = context.get('photo_array_json', UNDEFINED)
-        crumbs = context.get('crumbs', UNDEFINED)
-        translations = context.get('translations', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        _link = context.get('_link', UNDEFINED)
+        enable_comments = context.get('enable_comments', UNDEFINED)
+        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        photo_array_json = context.get('photo_array_json', UNDEFINED)
+        photo_array = context.get('photo_array', UNDEFINED)
         title = context.get('title', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
         post = context.get('post', UNDEFINED)
-        lang = context.get('lang', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        folders = context.get('folders', UNDEFINED)
+        def extra_js():
+            return render_extra_js(context._locals(__M_locals))
+        ui = _mako_get_namespace(context, 'ui')
+        translations = context.get('translations', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -102,18 +102,18 @@ def render_sourcelink(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        comments = _mako_get_namespace(context, 'comments')
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
-        folders = context.get('folders', UNDEFINED)
         permalink = context.get('permalink', UNDEFINED)
-        enable_comments = context.get('enable_comments', UNDEFINED)
         photo_array = context.get('photo_array', UNDEFINED)
-        crumbs = context.get('crumbs', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
         title = context.get('title', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
         post = context.get('post', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
         def content():
             return render_content(context)
+        folders = context.get('folders', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
+        enable_comments = context.get('enable_comments', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(ui.breadcrumbs(crumbs)))
@@ -161,14 +161,14 @@ def render_content(context,**pageargs):
 def render_extra_head(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        _link = context.get('_link', UNDEFINED)
         def extra_head():
             return render_extra_head(context)
-        gallery_path = context.get('gallery_path', UNDEFINED)
-        len = context.get('len', UNDEFINED)
-        translations = context.get('translations', UNDEFINED)
         parent = context.get('parent', UNDEFINED)
+        len = context.get('len', UNDEFINED)
         lang = context.get('lang', UNDEFINED)
+        gallery_path = context.get('gallery_path', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        translations = context.get('translations', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer(str(parent.extra_head()))
@@ -190,10 +190,10 @@ def render_extra_head(context,**pageargs):
 def render_extra_js(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def extra_js():
-            return render_extra_js(context)
         thumbnail_size = context.get('thumbnail_size', UNDEFINED)
         photo_array_json = context.get('photo_array_json', UNDEFINED)
+        def extra_js():
+            return render_extra_js(context)
         __M_writer = context.writer()
         __M_writer('\n<script src="/assets/js/justified-layout.min.js"></script>\n<script src="/assets/js/gallery.min.js"></script>\n<script>\nvar jsonContent = ')
         __M_writer(str(photo_array_json))
---
+++ posts/md-extensions-nikola.rst
@@ -0,0 +1,157 @@
+.. title: Use Markdown extensions in Nikola
+.. slug: md-extensions-nikola
+.. date: 2019-03-15 20:00:43 UTC+01:00
+.. tags: nikola,markdown
+.. category: tipps&tricks
+.. link: 
+.. description: 
+.. type: text
+
+.. contents::
+
+**The default markup language of input files in Nikola is restructuredText. You can, of course, configure Nikola to also process Markdown formatted files but by default there are missing some features like admonitions or table of contents which can be enabled by extensions.**
+
+Basic configuration
+*******************
+
+First things first: you have to edit the variables ``COMPILERS``, ``POSTS`` and ``PAGES`` of the *conf.py* and uncomment or add a line in order to make Nikola recognize and render Markdown files:
+
+.. code:: python
+
+    COMPILERS = {
+        "rest": ('.rst', '.txt'),
+        "markdown": ('.md', '.mdown', '.markdown'),
+        ...
+        }
+    POSTS = (
+        ("posts/*.rst", "posts", "post.tmpl"),
+        ...
+        ("posts/*.md", "posts", "post.tmpl"),
+    )
+    PAGES = (
+        ("pages/*.rst", "pages", "story.tmpl"),
+        ...
+        ("pages/*.md", "pages", "story.tmpl"),
+    )
+
+Markdown extensions
+*******************
+
+There are two types of Markdown extensions:
+
+1. `Officially supported extensions <https://python-markdown.github.io/extensions/#officially-supported-extensions>`_ that are shipped with the *python-markdown* package.
+
+2. `Third party extensions <https://github.com/Python-Markdown/markdown/wiki/Third-Party-Extensions>`_ which have to be installed separately on your system.
+
+Enable
+======
+
+An extension is enabled by adding it to the ``MARKDOWN_EXTENSIONS`` variable.
+
+If you want to enable officially supported extensions you add the extensions' *entry point* as list item. If the extension is part of the *extra* folder you can use ``extra`` as list item or the name of the extension, p.e. you enable definition lists with either ``def_list`` or ``extra``.
+
+The list item for a third party extension is the exact dot notation (see the extension's documentation).
+
+Examples
+++++++++
+
+.. code:: python
+
+    MARKDOWN_EXTENSIONS = [# all extensions of extra available
+                           "extra",
+                           # other officially supported extensions
+                           "admonition",
+                           "toc",
+                           # third party extension
+                           "markdown_include.include",
+                          ]
+
+Configure
+=========
+
+Some extensions offer configuration options. These are defined in the ``MARKDOWN_EXTENSIONS_CONFIGS`` variable. The dictionary's structure is
+
+.. code:: python
+
+    MARKDOWN_EXTENSIONS_CONFIGS = {
+        DEFAULT_LANG: {
+            "ext1": {"option1": value1,
+                     "option2": value2",
+                },
+            "ext2": {"option1": value1,
+                     "option2": value2",
+                },
+            ...
+        },
+        "other_configured_lang": {
+            "ext1": {"option1": value1,
+                     "option2": value3",
+                },
+            ...
+        },
+    }
+
+
+Use
+===
+
+Consult the extension's documentation for details. Useful extensions:
+
+Table of contents
++++++++++++++++++
+
+Enabled by:
+    "toc"
+
+Usage:
+    .. code::
+
+        [TOC]
+
+Definition lists
+++++++++++++++++
+
+Enabled by:
+    "extra"
+
+Usage:
+    .. code::
+
+        item1
+        :   text
+        item2
+        :   text
+
+Admonitions
++++++++++++
+
+Enabled by:
+    "admonition"
+
+Usage:
+    Common types in reST are the attention, caution, danger, error, hint, important, note, tip, and warning directives but any type can be defined (css editing may be required).
+    
+    .. code::
+
+        !!! danger
+            no trespassing
+
+        !!! important "custom title"
+            you better read this
+
+Include
++++++++
+
+Enabled by:
+    "markdown_include.include"
+
+Usage:
+    .. code::
+
+        {!posts/this-is-another-markdown-file.md!}
+
+    The input file will not be rendered if it has not been changed even if an included file was edited.
+
+.. admonition:: Fun fact
+
+    This posts has been written in restructuredText.
